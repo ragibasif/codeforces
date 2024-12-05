46e558f4924bbbd4e2fe0ab3daab6720c00523ab
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-12-05 18:46:25 -0500

    create file for next problem

---
+++ d2-a/beautiful-matrix/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void fast_io();
+void solve();
+
+void solve() {
+    fast_io();
+}
+
+int main(int argc, char* argv[]) {
+
+    solve();
+
+    return 0;
+}
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
