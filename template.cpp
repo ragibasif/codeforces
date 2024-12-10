a327d7335a9dfa136ab7b8796abfb123ba041902
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-12-09 19:31:31 -0500

    {{BACKUP}} Mon Dec  9 19:31:31 2024

--- template.cpp
+++ template.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void fast_io();
-void solve();
-
-void solve() {
-    fast_io();
-}
 
 int main(int argc, char* argv[]) {
 
-    solve();
+    // fast input/output
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     return 0;
 }
 
-void fast_io() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-}
