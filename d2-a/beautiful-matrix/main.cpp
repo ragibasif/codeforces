049870524184a4f27a55c94619ef51d7a5958be6
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-12-13 12:35:17 -0500

    solve beautiful matrix

--- d2-a/beautiful-matrix/main.cpp
+++ d2-a/beautiful-matrix/main.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
+#define MAX_ROW 5
+#define MAX_COL 5
+#define MIDDLE_ROW 2
+#define MIDDLE_COL 2
 using namespace std;
 
-void fast_io();
-void solve();
 
-void solve() {
-    fast_io();
-}
 
 int main(int argc, char* argv[]) {
 
-    solve();
-
-    return 0;
-}
-
-void fast_io() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
+    vector<vector<int>> matrix;
+
+    for (int i = 0; i < MAX_ROW; i++) {
+
+        for (int j = 0; j < MAX_COL; j++) {
+            int temp;
+            cin >> temp;
+            if (temp == 1) {
+                int result = abs(MIDDLE_ROW - i) + abs(MIDDLE_COL - j);
+                cout << result << endl;
+                return 0;
+            }
+        }
+    }
+
+
+
+
+    return 0;
 }
