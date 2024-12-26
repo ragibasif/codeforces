251df0024f1dec1d5ef1af10504362886dc5da76
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-12-26 09:37:13 -0500

    {{UPDATE}} Thu Dec 26 09:37:13 2024

---
+++ practice/buy-a-shovel/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+
+/* To run:
+   g++ -std=c++17 main.cpp && ./a.out
+   */
+
+using namespace std;
+
+void solve();
+
+/*Helper functions*/
+void fastIO();
+int power(int base, int exponent);
+int factorial(int number);
+bool divisible(int number, int divisor);
+int lastNum(int number);
+
+void solve() {
+    int k,r;
+    cin >> k >> r;
+    int counter = 1;
+    int temp = k;
+
+    while (true) {
+        if (lastNum(temp) == 0 || lastNum(temp) == r) {
+            cout << counter << endl;
+            return;
+        }
+        else {
+            counter++;
+            temp = counter * k;
+        }
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+
+    fastIO();
+    solve();
+    return 0;
+}
+
+/*Helper functions*/
+void fastIO() {
+    // fast input/output
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+bool divisible(int number, int divisor) {
+    if (divisor == 0) {
+        cerr << "Error: Division by zero is undefined." << endl;
+        return false;
+    }
+    return number % divisor == 0;
+}
+
+// n!
+int factorial(int number) {
+    if (number < 0) {
+        cerr << "Factorial is not defined for negative numbers." << endl;
+        return -1;
+    }
+
+    int result = 1;
+    for (int i = 1; i <= number; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+// integer power function to avoid floating point rounding errors
+int power(int base, int exponent) {
+    int result = 1;
+    for (int i = 0; i < exponent; ++i) {
+        result *= base;
+    }
+    return result;
+}
+
+// get the last digit of a given number (eg. 117 => 7, 234 => 4, 10 => 0)
+int lastNum(int number) {
+    while (number > 9) {
+        number %= 10;
+    }
+
+    return number;
+}
--- template/main.cpp
+++ template/main.cpp
@@ -6,33 +6,49 @@
 #include <unordered_map>
 #include <unordered_set>
 
-
-/* To run:
-g++ -std=c++17 main.cpp && ./a.out
-*/
-
 using namespace std;
 
 void solve();
+void solve2();
+void solve3();
+void solve4();
 
-/*Helper functions*/
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+
+// Utility functionsj
 void fastIO();
-int power(int base, int exponent);
-int factorial(int number);
-bool divisible(int number, int divisor);
-int lastNum(int number);
+long long power(long long base, long long exponent);
+long long factorial(long long number);
+bool isDivisible(long long number, long long divisor);
+long long lastDigit(long long number);
+long long gcd(long long a, long long b);
+long long lcm(long long a, long long b);
+long long modularExponentiation(long long base, long long exponent, long long mod);
+
 
 void solve() {
     int n;
     cin >> n;
-    cout << lastNum(n) << endl;
 }
 
-
+/*
+********************************************************************************
+g++ -std=c++11 -O2 -Wall main.cpp && ./a.out
+g++ -std=c++17 -O2 -Wall main.cpp && ./a.out
+g++ -std=c++20 -O2 -Wall main.cpp && ./a.out
+********************************************************************************
+*/
 int main(int argc, char* argv[]) {
 
     fastIO();
-    solve();
+    int t;
+    cin >> t; // number of test cases
+    while (t--) {
+        // will run as long as t is greater than 0
+        solve();
+    }
     return 0;
 }
 
@@ -44,7 +60,7 @@ void fastIO() {
     cout.tie(0);
 }
 
-bool divisible(int number, int divisor) {
+bool isDivisible(long long number, long long divisor) {
     if (divisor == 0) {
         cerr << "Error: Division by zero is undefined." << endl;
         return false;
@@ -53,33 +69,65 @@ bool divisible(int number, int divisor) {
 }
 
 // n!
-int factorial(int number) {
+long long factorial(long long number) {
     if (number < 0) {
         cerr << "Factorial is not defined for negative numbers." << endl;
         return -1;
     }
 
-    int result = 1;
-    for (int i = 1; i <= number; ++i) {
+    long long result = 1;
+    for (long long i = 1; i <= number; ++i) {
         result *= i;
     }
     return result;
 }
 
 // integer power function to avoid floating point rounding errors
-int power(int base, int exponent) {
-    int result = 1;
-    for (int i = 0; i < exponent; ++i) {
+long long power(long long base, long long exponent) {
+    long long result = 1;
+    for (long long i = 0; i < exponent; ++i) {
         result *= base;
     }
     return result;
 }
 
 // get the last digit of a given number (eg. 117 => 7, 234 => 4, 10 => 0)
-int lastNum(int number) {
+long long lastDigit(long long number) {
     while (number > 9) {
         number %= 10;
     }
 
     return number;
 }
+/*
+ * recursively calculate gcd using the eclidean algorithm
+ * */
+long long gcd(long long a, long long b) {
+    if (b == 0)
+        return a;
+    gcd(b, a % b);
+}
+
+/*
+ * find the least common multiple using gcd*/
+long long lcm(long long a, long long b) {
+    return (a / gcd(a,b)) * b;
+}
+
+/*
+ * uses exponentiation by squaring technique
+ * time: O(log exponent)*/
+long long modularExponentiation(long long base, long long exponent, long long mod) {
+    long long result = 1;
+    // while exponent > 0
+    while (exponent) {
+        if (exponent % 2 == 1) {
+            result = (result * base) % mod;
+        }
+        base = power(base,2) % mod;
+        exponent /= 2;
+    }
+    return result;
+}
+
+
